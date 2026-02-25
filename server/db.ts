58bc13ee7f94b71e6aa1d8b496dfcf6d8d49a8da
kotovskiigerman4-dot <kotovskiigerman4@gmail.com>
2026-02-24 19:06:58 -0500

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -10,5 +10,12 @@ if (!process.env.DATABASE_URL) {
   );
 }
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+// Добавляем настройки SSL, чтобы избежать ошибки "self-signed certificate"
+export const pool = new Pool({ 
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 export const db = drizzle(pool, { schema });
